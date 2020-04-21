817fb551ec598ffe24ad307a608bdbae7675d0b2
Steve Landherr <landherr@cs.wisc.edu>
2020-04-21 17:34:35 -0500

    Enhance extract-dataset.py to generate new features
    * Time-submitted bins quantify the number of submissions made over
    a fixed period of time. (Default: 10 minutes)
    * Bag-of-words vocabulary counts for the most frequently used words
    in the comment texts. (Default: top 100 words)
    
    Both are configurable in extract_spec.py, and can be disabled.

--- scripts/extract_spec.py
+++ scripts/extract_spec.py
@@ -5,15 +5,35 @@
 # Specification for what to extract from source datasets.
 # Customize this file to adjust dataset extraction.
 
-import standardize
+from lib import standardize
 
 # Maximum number of instances to extract.  None => extract all.
 # This is useful when testing changes to the spec.  It takes about
 # 30 minutes to perform a full extraction.
 # extract_max = 1000
+
 extract_max = None
 
-# These dictionaries define how to extract values from the source datasets
+# extract_submitted_bin_minutes - The time range in minutes over which to
+# count the number of submissions.  Set to None to disable.
+# This adds four features to each feature vector:
+# 'all_submitted_bin': The count of all submissions in the fcc-deidentified dataset
+#   that were submitted in the same extract_submitted_bin_minutes interval.
+# 'all_centered_bin': The count of all submissions in the fcc-deidentified dataset
+#   that were submitted in the interval centered at the time the item was submitted.
+# 'campaign_submitted_bin' and 'campaign_centered_bin': same as above, except only
+#   counting instances from the same campaign.
+
+extract_submitted_bin_minutes = 10
+
+# extract_bag_of_words_vocabulary - Create features in each feature vector
+# for the extract_bag_of_words_vocabulary number of most frequently seen words
+# in comments included in the extracted dataset.  Set to None to disable.
+# Features are named 'text.<word>'
+
+extract_bag_of_words_vocabulary = 100
+
+# The following dictionaries define how to extract values from the source datasets
 # and convert them into standardized features.
 # The key is the name of the feature. The value is another dictionary with the
 # following entries:
@@ -25,7 +45,6 @@ extract_max = None
 #               The middle list defines elements that are to be concatenated.
 #               The inner list defines nested elements in the source dictionary.
 # 'filter': A function used to standardize the feature value
-
 
 fcc_submitted_feature = {
     'one-hot': False,
---
+++ scripts/lib/dataset.py
@@ -0,0 +1,384 @@
+#!/usr/bin/python3
+#
+# Author: Steve Landherr <landherr@cs.wisc.edu>
+#
+import csv
+import glob
+import json
+import os
+import sys
+
+from extract_spec import *
+from lib import standardize
+
+# Location of the source datasets.  By default, look in 'datasets'
+# under the current working directory.  Can be changed with the
+# DATASETS_PATH environment variable.
+
+datasets_path = 'datasets'
+if 'DATASETS_PATH' in os.environ:
+    datasets_path = os.environ['DATASETS_PATH']
+survey_csv_path = os.path.join(datasets_path,
+                               'startuppolicylab',
+                               'deidentified_survey_results.csv')
+email_hash_index_path = os.path.join(datasets_path,
+                                     'indices',
+                                     'contact_email.json')
+fcc_deidentified_path = os.path.join(datasets_path,
+                                     'fcc-deidentified', 'v1')
+fcc_bag_of_words_path = os.path.join(datasets_path,
+                                     'fcc-bag-of-words', 'v1')
+standard_combined_path = os.path.join(datasets_path,
+                                      'standard-combined', 'example',
+                                      'dataset.csv')
+
+
+class Dataitem(dict):
+    """
+    Collects selected features from the source datasets
+    """
+    def __init__(self, _hash):
+        super().__init__()
+        if not _hash:
+            raise ValueError("empty hash")
+        self._hash = _hash
+        self._id = None
+
+    def add_features(self, row, features):
+        added_features = set()
+        for feature, element in features.items():
+            feature_value = standardize.element(element, row)
+            if element['one-hot']:
+                feature = feature + '.' + str(feature_value)
+                self[feature] = 1.0
+            else:
+                self[feature] = feature_value
+            added_features.add(feature)
+        return added_features
+
+    def add_bag_of_words(self, word_counts):
+        for word in word_counts:
+            feature = 'text.' + word
+            self[feature] = float(word_counts[word])
+
+    def add_all(self, row):
+        self.update(row)
+        return row.keys()
+
+    def get_hash(self):
+        return self._hash
+
+    def get_id(self):
+        return self._id
+
+    def set_id(self, _id):
+        self._id = _id
+
+    def get_campaign(self):
+        campaigns = [campaign_key for campaign_key in self if campaign_key.startswith('campaign.')]
+        if not campaigns:
+            return None
+        return campaigns[0]
+
+
+def load_email_index():
+    with open(email_hash_index_path) as index_file:
+        return json.load(index_file)
+
+
+def make_point_from_survey_row(row, line_num):
+    """
+    Create items from the Startup Policy Lab survey.
+    To be included, an item must have:
+     * a valid email_hash
+     * One of:
+       - a survey response (active email address)
+       - the email bounced (bogus / inactive email address)
+       - the email send failed (bogus email address)
+
+    :return: None
+    """
+    # Skip items without a valid e-mail hash.
+    if row['email_valid'] != 'True':
+        return None
+    try:
+        item = Dataitem(row['email_hash'])
+    except ValueError as exc:
+        sys.stderr.write("{}: {}\n".format(line_num, exc))
+        return None
+    item.add_features(row, survey_features)
+    # Skip items with no response and no indication of failure
+    if ((item['not_commenter'] in standardize.exception_values or item['not_commenter'] == 0.0) and
+            (item['bounced'] in standardize.exception_values or item['bounced'] != 1.0) and
+            (item['send_failed'] in standardize.exception_values or item['send_failed'] != 1.0)):
+        return None
+    return item
+
+
+class Dataset(list):
+    """
+    Used to compile a dataset extracted/generated from the source datasets.
+    """
+    def __init__(self):
+        super().__init__()
+        self.features = set()
+        self.by_hash = dict()
+        self.by_json = None
+        self.bins_by_campaign = dict()
+        self.all_bins = dict()
+        self.vocabulary = dict()
+
+    def initialize_from_csv(self, path, item_factory=None):
+        """
+        Create an initial set of data items based on data in
+        the specified CSV file.  The 'email_hash' feature is required.
+
+        :param path: Location of the CSV file
+        :param item_factory: Optional function to filter and standardize
+                             items found in the CSV file.  If not provided,
+                             all items are included verbatim.
+        :return:
+        """
+        sys.stderr.write(os.path.basename(survey_csv_path))
+        sys.stderr.flush()
+        with open(path, newline='') as csv_file:
+            reader = csv.DictReader(csv_file)
+            num_cols = len(reader.fieldnames)
+            for i, row in enumerate(reader):
+                if i % 10000 == 0:
+                    sys.stderr.write('.')
+                    sys.stderr.flush()
+                if len(row) == num_cols:
+                    if filter is not None:
+                        item = item_factory(row, reader.line_num)
+                        if item is None:
+                            continue
+                    else:
+                        item = Dataitem(row['email_hash'])
+                        item.add_all(row)
+                    # Build the master list of features (used for column headers)
+                    self.features.update(item.keys())
+                    # Build the master list of items
+                    self.append(item)
+                    # Build a mapping of email_hash to items containing that hash
+                    if item.get_hash() not in self.by_hash:
+                        self.by_hash[item.get_hash()] = list()
+                    self.by_hash[item.get_hash()].append(item)
+                    if extract_max is not None and len(self) == extract_max:
+                        break
+                else:
+                    sys.stderr.write("{}: malformed row\n".format(reader.line_num))
+        sys.stderr.write(" [done] {} items\n".format(len(self)))
+        sys.stderr.flush()
+
+    def pull_points_from_survey(self):
+        """
+        Create initial data set items from the Startup Policy Lab survey dataset.
+
+        :return: None
+        """
+        self.initialize_from_csv(survey_csv_path, item_factory=make_point_from_survey_row)
+
+    def _add_to_submitted_bins(self, submitted, campaign=None):
+        """
+        Updates submitted bin counts.
+
+        :param submitted: Timestamp when item was submitted (float)
+        :param campaign: One-hot campaign feature associated with the item (string)
+        :return: None
+        """
+        if extract_submitted_bin_minutes is None:
+            return
+        submitted_bin = int(submitted / 60) * 60
+        if campaign is not None:
+            if campaign not in self.bins_by_campaign:
+                self.bins_by_campaign[campaign] = dict()
+            campaign_bins = self.bins_by_campaign[campaign]
+            if submitted_bin not in campaign_bins:
+                campaign_bins[submitted_bin] = 0
+            campaign_bins[submitted_bin] += 1
+        if submitted_bin not in self.all_bins:
+            self.all_bins[submitted_bin] = 0
+        self.all_bins[submitted_bin] += 1
+
+    def add_features_from_bins(self):
+        """
+        Adds time-submitted bin count features to each item in the dataset.
+
+        :return: None
+        """
+        if extract_submitted_bin_minutes is None:
+            return
+        submitted_bin_seconds = 60 * extract_submitted_bin_minutes
+        for item in self:
+            submitted = int(item['submitted'])
+            submitted_bin_base = int(submitted / submitted_bin_seconds) * submitted_bin_seconds
+            centered_bin_base = int((submitted - (submitted_bin_seconds / 2)) / 60) * 60
+            campaign = item.get_campaign()
+            campaign_bins = self.bins_by_campaign[campaign]
+            item['campaign_submitted_bin'] = sum([campaign_bins[submitted_bin]
+                                                  for submitted_bin in range(submitted_bin_base,
+                                                                             submitted_bin_base + submitted_bin_seconds,
+                                                                             60)
+                                                  if submitted_bin in campaign_bins])
+            item['campaign_centered_bin'] = sum([campaign_bins[submitted_bin]
+                                                for submitted_bin in range(centered_bin_base,
+                                                                           centered_bin_base + submitted_bin_seconds,
+                                                                           60)
+                                                if submitted_bin in campaign_bins])
+            item['all_submitted_bin'] = sum([self.all_bins[submitted_bin]
+                                             for submitted_bin in range(submitted_bin_base,
+                                                                        submitted_bin_base + submitted_bin_seconds, 60)
+                                             if submitted_bin in self.all_bins])
+            item['all_centered_bin'] = sum([self.all_bins[submitted_bin]
+                                           for submitted_bin in range(centered_bin_base,
+                                                                      centered_bin_base + submitted_bin_seconds, 60)
+                                           if submitted_bin in self.all_bins])
+            self.features.update(['campaign_submitted_bin', 'campaign_centered_bin',
+                                  'all_submitted_bin', 'all_centered_bin'])
+
+    def add_features_from_fcc(self):
+        """
+        Add features from the FCC dataset to the dataset items.
+
+        :return: None
+        """
+        self._load_json_buckets()
+        for json_name in sorted(self.by_json):
+            survey_items_in_json = self.by_json[json_name]
+            sys.stderr.write(json_name)
+            sys.stderr.flush()
+            status = ' '
+            items_found_in_json = dict()
+            with open(os.path.join(fcc_deidentified_path, json_name)) as json_file:
+                for i, submission in enumerate(json.load(json_file)):
+                    if i % 1000 == 0:
+                        sys.stderr.write(status)
+                        sys.stderr.flush()
+                        status = '.'
+                    item = self._find_item(survey_items_in_json, submission)
+                    if item is not None:
+                        items_found_in_json[item.get_id()] = item
+                        self.features.update(item.add_features(submission, fcc_features))
+                        status = '+'
+
+            # Add bag-of-words features if specified.
+            if extract_bag_of_words_vocabulary is not None and items_found_in_json:
+                sys.stderr.write(status)
+                sys.stderr.flush()
+                status = ' '
+                with open(os.path.join(fcc_bag_of_words_path, json_name)) as json_file:
+                    for i, submission in enumerate(json.load(json_file)):
+                        if i % 1000 == 0:
+                            sys.stderr.write(status)
+                            sys.stderr.flush()
+                            status = '.'
+                        submission_id = int(submission['id_submission'])
+                        if submission_id in items_found_in_json:
+                            item = items_found_in_json[submission_id]
+                            item.add_bag_of_words(submission['bag-of-words'])
+                            self._update_vocabulary(submission['bag-of-words'])
+                            status = 'v'
+            sys.stderr.write(status + ' [done]\n')
+            sys.stderr.flush()
+
+        # Generate time-submitted bin features.
+        self.add_features_from_bins()
+
+        # Select the top vocabulary features to include.
+        self.add_features_from_vocabulary()
+
+    def _update_vocabulary(self, word_counts):
+        """
+        Update vocabulary word counts with counts from an included dataset item.
+
+        :param word_counts: item's bag-of-word counts
+        :return: None
+        """
+        for word in word_counts:
+            feature = 'text.' + word
+            if feature not in self.vocabulary:
+                self.vocabulary[feature] = 0
+            self.vocabulary[feature] += word_counts[word]
+
+    def add_features_from_vocabulary(self):
+        """
+        Add top most frequently seen words to the dataset features.
+
+        :return: None
+        """
+        if not extract_bag_of_words_vocabulary:
+            return
+        self.features.update(sorted(self.vocabulary, key=lambda word: self.vocabulary[word],
+                                    reverse=True)[:extract_bag_of_words_vocabulary])
+
+    def _initialize_all_json_buckets(self):
+        """
+        Causes all json files to be scanned instead of just those that reference
+        selected email_hash values.
+
+        :return: None
+        """
+        for json_name in [os.path.basename(json_path)
+                          for json_path in glob.glob(os.path.join(fcc_deidentified_path, '*'))]:
+            self.by_json[json_name] = dict()
+
+    def _load_json_buckets(self):
+        """
+        Build the mapping of json file names to Dataitems containing email_hashes found in those files.
+
+        :return: None
+        """
+        self.by_json = dict()
+        sys.stderr.write("loading json buckets ...")
+        sys.stderr.flush()
+        # Use the pre-built map of email_hash to json files that reference the hash
+        index = load_email_index()
+        if extract_submitted_bin_minutes:
+            self._initialize_all_json_buckets()
+        for item in self:
+            for json_name in index[item.get_hash()]:
+                if json_name not in self.by_json:
+                    self.by_json[json_name] = dict()
+                if item.get_hash() not in self.by_json[json_name]:
+                    self.by_json[json_name][item.get_hash()] = self.by_hash[item.get_hash()]
+
+        sys.stderr.write(' {} buckets loaded\n'.format(len(self.by_json)))
+        sys.stderr.flush()
+
+    def _find_item(self, hash_items, submission):
+        """
+        Find an unclaimed item with a matching email_hash and a submission datestamp within one second.
+        As a side-effected, updates time-submitted bins based on the submission being researched.
+
+        :param hash_items: items indexed by email_hash (dict)
+        :param submission: fcc submission data (dict)
+        :return: claimed item from hash_items
+        """
+        submission_hash = standardize.element(fcc_email_hash_feature, submission)
+        submission_date = standardize.element(fcc_submitted_feature, submission)
+        submission_id = standardize.element(fcc_id_feature, submission)
+        if submission_hash in hash_items:
+            for item in hash_items[submission_hash]:
+                if item.get_id() is None and abs(submission_date - item['submitted']) <= 1:
+                    item.set_id(submission_id)
+                    self._add_to_submitted_bins(submission_date, campaign=item.get_campaign())
+                    return item
+        self._add_to_submitted_bins(submission_date)
+        return None
+
+    def print_csv(self):
+        """
+        Write the extracted dataset as a CSV on stdout.
+
+        :return: None
+        """
+        fieldnames = sorted(self.features)
+        writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, extrasaction='ignore', restval=0.0)
+        writer.writeheader()
+        for item in self:
+            writer.writerow(item)
+            if item.get_id() is None:
+                raise RuntimeError("unmatched item")
+            if item['email_hash'] in standardize.exception_values:
+                raise RuntimeError("invalid hash")
